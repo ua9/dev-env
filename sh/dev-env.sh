00c10aa9b2d16ce6c42ea381ba41917480279e5b
Anton Marianov <anovmari@gmail.com>
2016-03-24 11:32:46 +0000

    Fix for linux

--- sh/dev-env.sh
+++ sh/dev-env.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 docker_machine_name='default'
+is_linux="$(uname | grep -i linux)"
 ip=''
 port=22
 image="dev-env"
 alias di='docker inspect --format '\''{{ .NetworkSettings.IPAddress }}'\'' '
 
-if [ "x$docker_machine_name" != 'x' ]
+if [ "x$is_linux" = "x" ]
 then
 	eval $(docker-machine env $docker_machine_name)
 fi
@@ -18,7 +19,7 @@ then
 	running_id=$(docker run -P -d $image)
 fi
 
-if [ "x$docker_machine_name" != 'x' ]
+if [ "x$is_linux" = "x" ]
 then
 	ip=$(docker-machine ip $docker_machine_name)
 	port=$(docker port $running_id 22 | sed s/0.0.0.0://)
@@ -26,4 +27,4 @@ else
 	ip=$(di $running_id)
 fi
 
-ssh -oStrictHostKeyChecking=no -t -p $port ubunt
+ssh -oStrictHostKeyChecking=no -t -p $port ubuntu@$ip screen -xR
