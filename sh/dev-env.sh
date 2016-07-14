b4c8942e9632687dfc3e49c54dc535993d01c356
Anton Marianov <anovmari@gmail.com>
2016-07-14 12:46:59 +0300

    Fixes for internet connection absence

--- sh/dev-env.sh
+++ sh/dev-env.sh
@@ -13,18 +13,37 @@ then
   ports='-P'
 fi
 
-running_id=$(docker ps --filter="ancestor=$image" --format="{{.ID}} {{.Command}} {{.Ports}}" | grep sshd | grep 22\/tcp | head -n 1 | awk '{print $1}')
+running_id=$(docker ps --filter="ancestor=$image" \
+  --format="{{.ID}} {{.Command}} {{.Ports}}" | grep sshd | \
+  grep 22\/tcp | head -n 1 | awk '{print $1}')
 
 if [ "x$running_id" = 'x' ]
 then
-  docker pull $image 1>&2
-  running_id=$(docker run $ports --dns=8.8.8.8 --dns=8.8.4.4 --hostname=DEV-ENV --cap-add=NET_ADMIN --device //dev/net/tun -e "TZ=Europe/Kiev" -v "//etc/../://host" -v "//etc/localtime://etc/localtime:ro" -v //var/run/docker.sock://var/run/docker.sock --user root --expose 22 -d $image //usr/sbin/sshd -D)
+  curl --silent --head https://registry.hub.docker.com > /dev/null
+  if [ $? -eq 0 ]
+  then
+    docker pull $image 1>&2
+  elif [ "$(docker images | grep $image | wc -l)" -eq '0' ]
+  then
+    echo "There is no Internet and image $image"
+    exit 1
+  fi
+  running_id=$(docker run $ports --dns=8.8.8.8 --dns=8.8.4.4 \
+    --hostname=DEV-ENV --cap-add=NET_ADMIN \
+    --device //dev/net/tun -e "TZ=Europe/Kiev" -v "//etc/../://host" \
+    -v "//etc/localtime://etc/localtime:ro" \
+    -v //var/run/docker.sock://var/run/docker.sock \
+    --user root --expose 22 -d $image //usr/sbin/sshd -D)
 fi
 
 if [ "x$is_linux" = "x" ]
 then
-  ip=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "22/tcp") 0).HostIp}}' $running_id)
-  port=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "22/tcp") 0).HostPort}}' $running_id)
+  ip=$(docker inspect \
+    --format='{{(index (index .NetworkSettings.Ports "22/tcp") 0).HostIp}}' \
+    $running_id)
+  port=$(docker inspect \
+    --format='{{(index (index .NetworkSettings.Ports "22/tcp") 0).HostPort}}' \
+    $running_id)
 else
   ip=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $running_id)
 fi
@@ -34,7 +53,8 @@ then
   echo "port: $port"
   read -p "Press [Enter] to exit..."
 else
-  command='ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile /dev/null" -t -p '$port' ubuntu@'$ip' "tmux -2 attach || tmux -2 new"'
+  command='ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile /dev/null" \
+    -t -p '$port' ubuntu@'$ip' "tmux -2 attach || tmux -2 new"'
   if [ -t 0 ]
   then
     eval "$command"
